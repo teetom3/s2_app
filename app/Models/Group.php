fc3d9da43150b8a8f319f5af775562b1f38267d1
thomas <thomas.zeryouh@goons.fr>
2026-04-13 12:47:45 +0200

    correction 1 act3

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -15,6 +15,6 @@ class Group extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withTimestamps();
     }
 }
---
+++ database/migrations/2026_04_13_104513_add_unique_to_group_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('group_user', function (Blueprint $table) {
+            $table->unique(['group_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('group_user', function (Blueprint $table) {
+            $table->dropUnique(['group_id', 'user_id']);
+        });
+    }
+};
